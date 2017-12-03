ef958faf816cf68b383612177004f1cc3dfb0249
Unknown <rdleiva@my.uri.edu>
2017-12-03 18:01:36 -0500

    edited frecVec()

--- language.cpp
+++ language.cpp
@@ -20,12 +20,13 @@ double similarity(std::vector<int> vec1, std::vector<int> vec2);
 
 const int VEC_LENGTH = pow(27,3);
 
+//FIGURE OUT READING FROM THE COMMAND LINE, IT'S NOT IN QUOTES SO IT'S LIKE A LINKED LIST
 int main(int argc, char *argv[]){
     int size = argc;
     if (size < 2) error("Must provide more at least 2 files!");
 
     for (int i = 1; i < size-2; i++){
-        std::string languageNames[i-1] = argv[i];
+        std::vector<std::string> languageNames[i-1] = argv[i];
     }
 
     std::vector<double> similarityVec(size-2);
@@ -38,6 +39,47 @@ int main(int argc, char *argv[]){
     }//end of for loop
     return 0;
 }//end of main
+
+/*
+Reads one character at a time from a file, and stores three letters at a time in a trigram string str
+Calls getIndex() for each trigram and adds one to the element in the frequency vector vec with the corresponding index
+Creates a frequency vector vec, and makes the number represent an index in the vector.
+
+returns the frequency vector.
+*/
+std::vector<int> freqVec(std::ifstream &infile){
+    /*
+    initialize a zero vector of size 27^3.
+    */
+    std::vector<int> vec(VEC_LENGTH, 0);
+    std::string str = "000";
+    if (!infile.fail()){
+        while (infile.get(ch)){
+            if (ch == '\0') continue; //does continue make it skip that reding and go to the next loop of the while
+            else if (str[0] == '0') str[0] = ch;
+            else if (str[1] == '0') str[1] = ch;
+            else {
+                str[2] = ch;
+                int index = getIndex(str);
+                vec[index] += 1;
+                str[0] = str[1];
+                str[1] = str[2];
+            }
+        } //end of while loop
+        infile.close();
+    } else {
+        error("Could not open file");
+    }
+
+    return vec;
+} //end of freqVec
+
+/*
+Converts the trigram that is passed to the function as a string into a number with base 27,
+which will represent the index for that specific trigram.
+
+returns the index.
+*/
 
 int getIndex (std::string str){
     for (int i = 0; i < 3; i++){
@@ -53,7 +95,6 @@ int getIndex (std::string str){
         Since we are only using the lower case alphabet we reset the decimal values for each letter,
         so it starts at one and increments with each letter.
         Before, space is set equal to 96 which makes space equal to zero after we reset everything.
-        After converting the trigram to a number, one is added to the vector with the corresponding index.
         */
         index = (str[i] - 96) * pow(27, 2) + (str[i+1] - 96) * 27 + (str[i+2] - 96);
     }//end of for loop
@@ -61,39 +102,10 @@ int getIndex (std::string str){
     return index;
 }
 
-
-/*
-Converts each trigram that appears in a string into a number index and counts how many times each number appears.
-Creates a frequency vector vec, and makes the number represent an index in the vector.
-Loops through each trigram until the last trigram, when the index is length - 2.
-
-returns the frequency vector.
-*/
-std::vector<int> freqVec(std::ifstream &infile){
-    /*
-    initialize a zero vector of size 27^3.
-    */
-
-    std::vector<int> vec(VEC_LENGTH, 0);
-    if (!infile.fail()){
-        while (infile.get(ch)){
-
-
-            vec[index] += 1;
-
-        } //end of while loop
-        infile.close();
-    } else {
-        error("Could not open file")
-    }
-
-    return vec;
-} //end of freqVec
-
 double similarity(std::vector<int> vec1, std::vector<int> vec2){
 
     double nom = 0.0, denA = 0.0, denB = 0.0;
-    for (int i = 0; i < pow(27,3); i++){ //++i?
+    for (int i = 0; i < VEC_LENGTH; i++){ //++i?
         nom += vec1[i] * vec2[i];
         denA += pow(vec1[i], 2);
         denB += pow(vec2[i], 2);
