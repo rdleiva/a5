5a7516043cbb3a7d58755cf6c03c9982bc08446d
Unknown <rdleiva@my.uri.edu>
2017-12-09 14:42:02 -0500

    similarity() fixed, freqVec() redesigned, comments added and revised.
    Everything is all done.

--- language.cpp
+++ language.cpp
@@ -1,8 +1,12 @@
 /*
-Final Project
-Authors: Eydis Embla Ludviksdottir
-         Rotman Daniel rdleiva
-File: language.cpp converts a string into a sparse frequency vector and outputs it.
+CSC 211
+Final Project   12/11/2017
+Professor:  Noah Daniels
+Authors:    Eydis Embla Ludviksdottir, eydisembla@my.uri.edu
+            Rotman Daniel, rdleiva@my.uri.edu
+File: language.cpp
+      Compares one unknown language file to at least one known language file,
+      and determines which file the unknown one is most similar to.
 */
 #include <iostream>
 #include <cstdlib>
@@ -14,7 +18,6 @@ File: language.cpp converts a string into a sparse frequency vector and outputs 
 /*
 Function Prototypes
 */
-
 std::vector<int> freqVec(std::ifstream &infile);
 void error(std::string msg);
 double similarity(std::vector<int> vec1, std::vector<int> vec2);
@@ -22,64 +25,62 @@ int getIndex (std::string str);
 
 const int VEC_LENGTH = 27 * 27 * 27;
 
-
 int main(int argc, char *argv[]){
     int size = argc;
     if (size < 2) error("Must provide at least 2 files!");
 
-    char* testFile = argv[size-1];
-
-    std::vector<char*> langNames;
-
     /*
-    looping through the command line arguments except the first and the last one
-    and pushing it to the vector langNames
+    looping through the command line arguments except the first and the last one,
+    and store them in a vector, langNames.
     */
+    std::vector<char*> langNames;
     for(int i = 1; i < size-1; i++){
         char* str = argv[i];
         langNames.push_back(str);
     }
 
     /*
-    ifstream f(argv[i]);
-    vector = getfreq(&f);
+    making a freguency vector for the last command line argument file, test file.
     */
-
-    //std::cout << argv[1] << std::endl;
-    //std::cout << testFile << std::endl;
-    //std::cout << langNames[0] << std::endl;
-
-    std::vector<double> similarityVec;
+    char* testFile = argv[size-1];
     std::ifstream f;
     f.open(testFile);
-    //std::cout << testFile << std::endl;
     std::vector<int> v1 = freqVec(f);
     f.close();
 
+    /*
+    looping through the langNames vector, opening each file and making a trigram frequency vector.
+    Then calculating the cosine similarity of that vector and the test file vector, and store it in a similarity vector.
+    */
+    std::vector<double> similarityVec;
     for(size_t i = 0; i < langNames.size(); i++){
         std::ifstream fi;
         fi.open(langNames[i]);
-        //std::cout << langNames[i] << std::endl;
         std::vector<int> v2 = freqVec(fi);
         similarityVec.push_back(similarity(v1, v2));
         fi.close();
     }
 
     /*
-    looping through the similarityVec to find the greatest value ??
-    Need to find another way to find the most similar value..
-    find the one closes to 1.
+    looping through the similarityVec to find greatest value,
+    storing the index of the greatest value as n.
     */
-    int m = 2;
-    for(size_t i = 1; i < similarityVec.size() ; i++){
-        if(similarityVec[i]>m)
-        m = i;
-    }
-    std::cout<< langNames[m] << std::endl;;
+    double m = similarityVec[0];
+    int n = 0;
+    for(size_t i = 1; i < similarityVec.size(); i++){
+        if (similarityVec[i] > m){
+            m = similarityVec[i];
+            n = i;
+        } //end of if statement
+    } //end of for loop
 
-    //}//end of for loop
+    /*
+    outputting the file name with the index that represented the greatest value in the similarity vector.
+    */
+    std::cout<< langNames[n] << std::endl;;
+
     return 0;
-}//end of main
+} //end of main
 
 /*
 Reads one character at a time from a file, and stores three letters at a time in a trigram string str
@@ -88,34 +89,39 @@ Creates a frequency vector vec, and makes the number represent an index in the v
 
 returns the frequency vector.
 */
-
 std::vector<int> freqVec(std::ifstream &infile){
     /*
-    initialize a zero vector of size 27^3.
+    initialize a zero vector of size 27^3,
+    and a basecase trigram string of three zeros.
     */
-    std::vector<int> vec(VEC_LENGTH, 0);
-    std::string str = "000";
+    std::vector<int> freqVec(VEC_LENGTH, 0);
+    std::string trigram = "000";
     char ch;
+
+    /*
+    Reading one character at a time from the file,
+    */
     if (!infile.fail()){
+        /*
+        While reading from infile move the trigram "000" one character at a time along the infile,
+        starting from the first character of the language file all the way to the last one.
+        Get the index of each trigram, if there are no zeros in the trigram,
+        and add one to the element of the frequency vector with the corresponding index.
+        */
         while (infile.get(ch)){
-            if (ch == '\0') continue;
-            else if (str[0] == '0') str[0] = ch;
-            else if (str[1] == '0') str[1] = ch;
-            else {
-                str[2] = ch;
-                //std::cout << str << std::endl;
-                int index = getIndex(str);
-                //std::cout << index << std::endl;
-                vec[index] += 1;
-                str[0] = str[1];
-                str[1] = str[2];
+          if (ch == '\n') break;
+          trigram.push_back(ch);
+          trigram.erase(trigram.begin());
+          if (trigram[0] != '0'){
+              int index = getIndex(trigram);
+              freqVec[index] += 1;
             }
         } //end of while loop
     } else {
         error("Could not open file");
     }
 
-    return vec;
+    return freqVec;
 } //end of freqVec
 
 /*
@@ -124,7 +130,6 @@ which will represent the index for that specific trigram.
 
 returns the index.
 */
-
 int getIndex (std::string str){
     int index = 0;
     /*
@@ -151,23 +156,19 @@ and then combine the values together in the end as numerator over the product of
 
 returns the similarity value
 */
-
 double similarity(std::vector<int> vec1, std::vector<int> vec2){
 
     /*
-    looping through the frequency vector,
-    numerator: adds the products of the elements in the vectors with the same indexes.
-    denominator 1: adds the squared element in vec1.
-    denominator 2: adds the squared element in vec2.
+    looping through the frequency vector, computing the numerator and the two parts of the denominator.
     */
-    unsigned long long num = 0.0, den1 = 0.0, den2 = 0.0;
-    for (int i = 0; i < VEC_LENGTH; i++){ //++i?
+    unsigned long long num = 0, den1 = 0, den2 = 0;
+    for (size_t i = 0; i < VEC_LENGTH; i++){
         num += vec1[i] * vec2[i];
-        den1 += pow(vec1[i], 2);
-        den2 += pow(vec2[i], 2);
+        den1 += vec1[i] * vec1[i];
+        den2 += vec2[i] * vec2[i];
     } //end of for loop
 
-    return num / (sqrt(den1) * sqrt(den2));
+    return ((double) num / ((double)(sqrt(den1) * sqrt(den2))));
 } //end of similarity
 
 /*
